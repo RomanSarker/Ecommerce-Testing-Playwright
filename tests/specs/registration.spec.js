6bcc3c8d98332c2b4e3b5ff86053377aecacdd1d
Roman Sarker <romansarker2015@gmail.com>
2026-01-15 17:45:05 +0600

    Adding Timeout to get more understandable view

--- tests/specs/registration.spec.js
+++ tests/specs/registration.spec.js
@@ -5,13 +5,17 @@ test.describe("Evershop Ecommerce Registration Feature", ()=>{
     test("Successfully Register with valid Information",async({page})=>{
         const registration = new Registration(page);
         await page.goto("https://demo.evershop.io/");
+        // await page.pause();
         await registration.clickOnaccountIcon();
         await registration.clickOncreateAccountLink();
         await registration.enterfullNameInput("Roman14");
-        // await this.page.waitForTimeout(5000);
+        await page.waitForTimeout(2000);
         await registration.enteremailInput("roman14@email.com");
+        await page.waitForTimeout(2000);
         await registration.enterpasswordInput("Password123");
+        await page.waitForTimeout(2000);
         await registration.clickOnbuttonSignUp()
+        await page.waitForTimeout(2000);
        
     } )
     test("UnSuccessfully Register with valid Information",async()=>{
