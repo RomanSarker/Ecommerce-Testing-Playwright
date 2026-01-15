f129d5fbab50b331af8dbc0f926cbae01ebc9ea3
Roman Sarker <romansarker2015@gmail.com>
2026-01-15 13:24:33 +0600

    Registration Action page started

--- tests/pageObjects/registration/regActions.js
+++ tests/pageObjects/registration/regActions.js
@@ -3,7 +3,14 @@ import{RegistrationObjects} from "./regObjects";
 export class Registration{
     constructor(page){
         this.page = page;
-        
+        this.locator = new RegistrationObjects(page)
     }
+    async clickOnaccountIcon(){
+        await this.locator.accountIcon.click();
     }
-}
+    async clickOncreateAccountLink(){
+        await this.locator.createAccountLink.click();
+    }
+
+
+    }
